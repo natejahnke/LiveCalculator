a77ea3b340b81b27cbec15f3e7304ea681ca1ccb
Nathan Jahnke <naterased@gmail.com>
2019-06-04 15:13:59 -0500

    testing environment variables in netlify for firebase keys.

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,21 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyAsGjrbY97mZ7AYU28kt00RNDHHyzy9opg",
-  authDomain: "live-calculator.firebaseapp.com",
-  databaseURL: "https://live-calculator.firebaseio.com",
-  projectId: "live-calculator",
-  storageBucket: "live-calculator.appspot.com",
-  messagingSenderId: "594021649148",
-  appId: "1:594021649148:web:27a69b66a9873bce"
+  // apiKey: "AIzaSyAsGjrbY97mZ7AYU28kt00RNDHHyzy9opg",
+  // authDomain: "live-calculator.firebaseapp.com",
+  // databaseURL: "https://live-calculator.firebaseio.com",
+  // projectId: "live-calculator",
+  // storageBucket: "live-calculator.appspot.com",
+  // messagingSenderId: "594021649148",
+  // appId: "1:594021649148:web:27a69b66a9873bce"
+
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 var fire = firebase.initializeApp(config);

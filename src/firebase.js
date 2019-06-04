b91e65de7368fcd92ed6380d1b491da05b4e7c12
Nathan Jahnke <naterased@gmail.com>
2019-06-04 15:25:46 -0500

    further testing with variables with netlify.

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ const config = {
   // messagingSenderId: "594021649148",
   // appId: "1:594021649148:web:27a69b66a9873bce"
 
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
 };
 
 var fire = firebase.initializeApp(config);
